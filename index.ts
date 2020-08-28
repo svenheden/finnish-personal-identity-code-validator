ed0482590932668a3dd49f97b037b4bd6a7c5103
Jonathan Svenheden <jonathan@svenheden.com>
2020-08-28 16:25:04 +0200

    Clean up index.ts a bit

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 const pattern = /^[0-3]\d[0-1]\d{3}[\-+A]\d{3}[\dABCDEFHJKLMNPRSTUVWXY]$/i;
-const controlChars = '0123456789ABCDEFHJKLMNPRSTUVWXY'.split('');
+const controlChars = "0123456789ABCDEFHJKLMNPRSTUVWXY".split("");
+const centurySigns = { "+": 1800, "-": 1900, A: 2000 };
 
 const hasCorrectChecksum = (input: string, checksum: string) => {
   const remainder = Number(input) % 31;
@@ -8,9 +9,14 @@ const hasCorrectChecksum = (input: string, checksum: string) => {
 };
 
 const hasValidDate = (input: string) => {
-  let [_, dayStr, monthStr, yearStr, centuryChar] = /^(\d{2})(\d{2})(\d{2})(.)/.exec(input);
-  const centuryMap = { '+': 1800, '-': 1900, A: 2000 }
-  const year = Number(yearStr) + centuryMap[centuryChar];
+  const [
+    _,
+    dayStr,
+    monthStr,
+    yearStr,
+    centurySign,
+  ] = /^(\d{2})(\d{2})(\d{2})(.)/.exec(input);
+  const year = Number(yearStr) + centurySigns[centurySign];
   const month = Number(monthStr) - 1;
   const day = Number(dayStr);
   const date = new Date(year, month, day);
@@ -18,7 +24,7 @@ const hasValidDate = (input: string) => {
   const yearIsValid = String(date.getFullYear()).substr(-2) === yearStr;
   const monthIsValid = date.getMonth() === month;
   const dayIsValid = date.getDate() === day;
-  console.log(dayIsValid, monthIsValid, yearIsValid, date)
+
   return yearIsValid && monthIsValid && dayIsValid;
 };
 
@@ -27,7 +33,7 @@ export const isValid = (input: string) => {
     return false;
   }
 
-  const cleaned = input.substr(0, 10).replace(/\D/g, '');
+  const cleaned = input.substr(0, 10).replace(/\D/g, "");
   const checksum = input.substr(-1).toUpperCase();
 
   return hasCorrectChecksum(cleaned, checksum) && hasValidDate(input);
