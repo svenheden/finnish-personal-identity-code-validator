92110581d4b37643a9d7a23bf012497d4932110c
Tuomas Tuominen <tuomas.tuominen@bisnode.com>
2020-08-28 11:34:29 +0300

    Fix bug in leap year handling

--- index.ts
+++ index.ts
@@ -8,9 +8,9 @@ const hasCorrectChecksum = (input: string, checksum: string) => {
 };
 
 const hasValidDate = (input: string) => {
-  let [_, dayStr, monthStr, yearStr] = /^(\d{2})(\d{2})(\d{2})/.exec(input);
-
-  const year = Number(yearStr);
+  let [_, dayStr, monthStr, yearStr, centuryChar] = /^(\d{2})(\d{2})(\d{2})(.)/.exec(input);
+  const centuryMap = { '+': 1800, '-': 1900, A: 2000 }
+  const year = Number(yearStr) + centuryMap[centuryChar];
   const month = Number(monthStr) - 1;
   const day = Number(dayStr);
   const date = new Date(year, month, day);
@@ -18,7 +18,7 @@ const hasValidDate = (input: string) => {
   const yearIsValid = String(date.getFullYear()).substr(-2) === yearStr;
   const monthIsValid = date.getMonth() === month;
   const dayIsValid = date.getDate() === day;
-
+  console.log(dayIsValid, monthIsValid, yearIsValid, date)
   return yearIsValid && monthIsValid && dayIsValid;
 };
 
@@ -30,5 +30,5 @@ export const isValid = (input: string) => {
   const cleaned = input.substr(0, 10).replace(/\D/g, '');
   const checksum = input.substr(-1).toUpperCase();
 
-  return hasCorrectChecksum(cleaned, checksum) && hasValidDate(cleaned);
+  return hasCorrectChecksum(cleaned, checksum) && hasValidDate(input);
 };
